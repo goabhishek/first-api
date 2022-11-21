3db8bfb890be308694851df108f7c9cd9d4ad3f8
goabhishek <mabhishek4156@gmail.com>
2022-11-21 18:34:38 +0530

    add routes

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+dotenv.config();
 
-app.use('/', (req, res) => {
-  console.log('this is main url');
-});
+app.use(express.json());
+
+mongoose
+  .connect(
+    process.env.MONGO_URL,
+    'mongodb+srv://goabhishek:<password>@cluster0.oizf2sf.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(console.log('Connected to mongodb'))
+  .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute);
 
 app.listen('5000', () => {
   console.log('backend is running');
